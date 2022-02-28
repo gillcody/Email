6860a2078d0f95b820b53ad729e200deefcb9ab6
Cody Gill <byq195@my.utsa.edu>
2022-02-28 07:59:27 -0600

    Fixed password generator; Added alt email

--- src/main/java/EmailApp.java
+++ src/main/java/EmailApp.java
@@ -4,5 +4,8 @@ public class EmailApp {
 
     public static void main(String[] args) {
         Email email = new Email("Cody", "Gill");
+
+        email.setAltEmail("Alternate email: cody.gill@gmail.com");
+        System.out.println(email.getAltEmail());
     }
 }
